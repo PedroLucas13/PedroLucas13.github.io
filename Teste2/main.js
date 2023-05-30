1e3d97ab94d330b1a5692bbf075fa70cc96c372d
PedroLucas13 <76547200+PedroLucas13@users.noreply.github.com>
2023-05-30 00:18:26 -0300

    Add files via upload

---
+++ Teste2/main.js
@@ -0,0 +1,9 @@
+function aYes(){
+    var rTopNumber = Math.floor(Math.random() * 100) + 30;
+    var rRightNumber = Math.floor(Math.random() * 100) + 30;
+    document.querySelector(".yes").setAttribute("style", `top:${rTopNumber}px; right:${rRightNumber}px`)
+}
+function aNo(){
+    document.querySelector(".texto").setAttribute("style", "display:none");
+    document.querySelector(".video").setAttribute("style", "display:block")
+}
