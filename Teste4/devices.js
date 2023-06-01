c544975a7f1f54f3e0503bcbf9aacf2bfd3327a0
PedroLucas13 <76547200+PedroLucas13@users.noreply.github.com>
2023-05-31 21:55:36 -0300

    Update devices.js

--- Teste4/devices.js
+++ Teste4/devices.js
@@ -1,33 +1,4 @@
-/**
- * Copyright 2020 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the 'License');
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an 'AS IS' BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-
-// This function's role is to enable smooth transition to the brave new world of
-// User-Agent Client Hints. If you have legacy code that relies on
-// `navigator.userAgent` and which relies on entropy that will go away by
-// default, you *need* to refactor it to use UA-CH. This function is to be used
-// as a stop gap, to enable smooth transition during that period.
-
-/**
-* @param {string[]} hints
-* @return {Promise<string|undefined>} A Promise that resolves to a string if a
-*   UA could be synthesized from client hints, otherwise undefined.
-*/
 async function getUserAgentUsingClientHints(hints) {
-    // Helper functions for platform specific strings
     const GetCrosSpecificString = (values) => {
       let osCPUFragment = "";
       if (values.bitness == "64") {
@@ -105,8 +76,6 @@ async function getUserAgentUsingClientHints(hints) {
     if (!navigator.userAgentData) {
       return Promise.resolve();
     }
-  
-    // Verify that this is a Chromium-based browser
     let is_chromium = false;
     let chromium_version;
     const is_chrome_ua_pattern = new RegExp(
@@ -114,21 +83,14 @@ async function getUserAgentUsingClientHints(hints) {
     );
     navigator.userAgentData.brands.forEach((value) => {
       if (value.brand == "Chromium") {
-        // Let's double check the UA string as well, so we don't accidentally
-        // capture a headless browser or friendly bot (which should report as
-        // HeadlessChrome or something entirely different).
         is_chromium = is_chrome_ua_pattern.test(navigator.userAgent);
         chromium_version = value.version;
       }
     });
     if (!is_chromium || chromium_version < 100) {
-      // If this is not a Chromium-based browser, the UA string should be very
-      // different. Or, if this is a Chromium lower than 100, it doesn't have
-      // all the hints we rely on. So let's bail.
       return Promise.resolve();
     }
   
-    // Main logic
     return new Promise((resolve) => {
       navigator.userAgentData.getHighEntropyValues(hints).then((values) => {
         let initialValues = {
@@ -161,8 +123,6 @@ async function getUserAgentUsingClientHints(hints) {
   }
   
   function getVersion(fullVersionList, majorVersion) {
-    // If we don't get a fullVersionList, or it's somehow undefined, return
-    // the reduced version number.
     return (
       fullVersionList?.find((item) => item.brand == "Google Chrome")?.version ||
       `${majorVersion}.0.0.0`
@@ -170,31 +130,22 @@ async function getUserAgentUsingClientHints(hints) {
   }
   
   function getWindowsPlatformVersion(platformVersion) {
-    // https://wicg.github.io/ua-client-hints/#get-the-legacy-windows-version-number
     const versionMap = new Map([
-      ["0.3.0", "6.3"], // Windows 8.1
-      ["0.2.0", "6.2"], // Windows 8
-      ["0.1.0", "6.1"], // Windows 7
+      ["0.3.0", "6.3"],
+      ["0.2.0", "6.2"],
+      ["0.1.0", "6.1"],
     ]);
   
     if (versionMap.has(platformVersion)) {
       return versionMap.get(platformVersion);
     }
-  
-    // Windows 10 and above send "Windows NT 10.0"
     return "10.0";
   }
   
-  /**
-   * @param {string[]} hints
-   * @return {Promise<string|undefined>} A Promise that resolves on overriding the
-   *   navigator.userAgent string.
-   */
   async function overrideUserAgentUsingClientHints(hints) {
     return new Promise((resolve) => {
       getUserAgentUsingClientHints(hints).then((newUA) => {
         if (newUA) {
-          // Got a new UA value. Now override `navigator.userAgent`.
           Object.defineProperty(navigator, "userAgent", {
             value: newUA,
             writable: false,
